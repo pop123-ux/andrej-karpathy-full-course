ec0300176e71c40d2182da52d2a3fb77856c922f
Pop Alexandru <alexandrupp55@gmail.com>
2026-09-13 15:14:35 +0300

    added dropout to CausalSelfAttention and MLP classes + fixed bugs

--- nanogpt-lecture/GPT2_from_scratch - Andrej Karpathy.py
+++ nanogpt-lecture/GPT2_from_scratch - Andrej Karpathy.py
@@ -4,17 +4,14 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-batch_size=64
-block_size=256
-max_iters=5000
-eval_interval=500
-learning_rate=3e-4
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-eval_iters = 200
-n_embd=384
-n_head=6
-n_layer=6
-dropout=0.2
+@dataclass
+class GPTConfig:
+    block_size: int = 1024 # max sequence length
+    vocab_size: int = 50257 # number of tokens: 50,000 BPE merges + 256 bytes tokens + 1 <|endoftext|> token
+    n_layer: int = 12 # number of layers
+    n_head: int = 12 # number of heads
+    n_embd: int = 768 # embedding dimension
+    dropout: float = 0.2
 
 class CausalSelfAttention(nn.Module):
 
@@ -31,6 +28,9 @@ class CausalSelfAttention(nn.Module):
         # not really a bias, more of a mask, but following the OpenAI/HF naming
         self.register_buffer('bias', torch.tril(torch.ones(config.block_size, config.block_size)).view(1, 1, config.block_size, config.block_size))
 
+        self.attn_drop = nn.Dropout(config.dropout)
+        self.resid_drop = nn.Dropout(config.dropout)
+
     def forward(self, x):
         B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
         # calculate query, key, values for all heads in batch and move head forward
@@ -43,11 +43,13 @@ class CausalSelfAttention(nn.Module):
         att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
         att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
         att = F.softmax(att, dim=-1)
+        att = self.attn_drop(att)
         y = att @ v
         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
         # output projection
-        y = self.c_proj(y)
+        y = self.resid_drop(self.c_proj(y))
         return y
+        
 class MLP(nn.Module):
     
     def __init__(self, config):
@@ -55,11 +57,13 @@ class MLP(nn.Module):
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu = nn.GELU(approximate='tanh')
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.dropout = nn.Dropout(config.dropout)
 
     def forward(self, x):
         x = self.c_fc(x)
         x = self.gelu(x)
         x = self.c_proj(x)
+        x = self.dropout(x)
         return x
     
 class Block(nn.Module):
@@ -75,14 +79,6 @@ class Block(nn.Module):
         x = x + self.attn(self.ln_1(x))
         x = x + self.mlp(self.ln_2(x))
         return x
-    
-@dataclass
-class GPTConfig:
-    block_size: int = 1024 # max sequence length
-    vocab_size: int = 50257 # number of tokens: 50,000 BPE merges + 256 bytes tokens + 1 <|endoftext|> token
-    n_layer: int = 12 # number of layers
-    n_head: int = 12 # number of heads
-    n_embd: int = 768 # embedding dimension
 
 class GPT(nn.Module):
 
@@ -125,7 +121,7 @@ class GPT(nn.Module):
         config_args = {
             'gpt2': dict(n_layer=12, n_head=12, n_embd=768), # 124M params
             'gpt2-medium': dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
-            'gpt2-largs': dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
+            'gpt2-large': dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
             'gpt2-xl': dict(n_layer=48, n_head=25, n_embd=1600) # 1558M params
         }[model_type]
         config_args['vocab_size'] = 50257
@@ -182,7 +178,7 @@ enc = tiktoken.get_encoding('gpt2')
 tokens = enc.encode("Hello, I'm a language model,")
 tokens = torch.tensor(tokens, dtype=torch.long) # (8,)
 tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) # (5, 8)
-x = tokens.to('cuda')
+x = tokens.to(device)
 
 # generate!
 torch.manual_seed(42)
